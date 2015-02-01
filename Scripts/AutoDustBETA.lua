9bc4eed0b1a1fd6f0c0884b96bcca7b6a43a27d5
NovaHF <nova@live.co.uk>
2015-02-01 04:14:42 +0000

    Update
    Should reduce lag, haven't tested it, but should work, if not, pm me.

--- Scripts/AutoDustBETA.lua
+++ Scripts/AutoDustBETA.lua
@@ -44,55 +44,37 @@ function Main(tick)
 	if not me then return end
 	local dust = me:FindItem("item_dust")
 	
-	FindTarget()
-    	if target and (target:DoesHaveModifier("modifier_bounty_hunter_wind_walk") or target:DoesHaveModifier("modifier_riki_permanent_invisibility") or target:DoesHaveModifier("modifier_mirana_moonlight_shadow") or target:DoesHaveModifier("modifier_treant_natures_guise") or target:DoesHaveModifier("modifier_weaver_shukuchi") or target:DoesHaveModifier("modifier_broodmother_spin_web_invisible_applier") or target:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") or target:DoesHaveModifier("modifier_rune_invis") or target:DoesHaveModifier("modifier_clinkz_wind_walk") or target:DoesHaveModifier("modifier_item_shadow_amulet_fade")) and not (target:DoesHaveModifier("modifier_bounty_hunter_track") or target:DoesHaveModifier("modifier_bloodseeker_thirst_vision") or target:DoesHaveModifier("modifier_slardar_amplify_damage") or target:DoesHaveModifier("modifier_item_dustofappearance")) then
-	        if dust and dust:CanBeCasted() then
+        if dust and dust:CanBeCasted() then
+        	FindTarget()
+    	        if target and (target:DoesHaveModifier("modifier_bounty_hunter_wind_walk") or target:DoesHaveModifier("modifier_riki_permanent_invisibility") or target:DoesHaveModifier("modifier_mirana_moonlight_shadow") or target:DoesHaveModifier("modifier_treant_natures_guise") or target:DoesHaveModifier("modifier_weaver_shukuchi") or target:DoesHaveModifier("modifier_broodmother_spin_web_invisible_applier") or target:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") or target:DoesHaveModifier("modifier_rune_invis") or target:DoesHaveModifier("modifier_clinkz_wind_walk") or target:DoesHaveModifier("modifier_item_shadow_amulet_fade")) and not (target:DoesHaveModifier("modifier_bounty_hunter_track") or target:DoesHaveModifier("modifier_bloodseeker_thirst_vision") or target:DoesHaveModifier("modifier_slardar_amplify_damage") or target:DoesHaveModifier("modifier_item_dustofappearance")) then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-				return
-			else
-			    return
-		    end
-     	end
+			return
+	        end
 		
 		if me and me:DoesHaveModifier("modifier_invoker_ghost_walk_enemy") then
-	        if dust and dust:CanBeCasted() then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-				return
-			else
-			    return
-		    end
-     	end
+			return
+		end
 		
 		if target and target.name == ("npc_dota_hero_templar_assassin") and (target.health/target.maxHealth < 0.3) then
-	        if dust and dust:CanBeCasted() then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-				return
-			else
-			    return
-		    end
-     	end
+			return
+		end
 		
-		if target and target.name == ("npc_dota_hero_sand_king") and (target.health/target.maxHealth < 0.5) then
-	        if dust and dust:CanBeCasted() then
+		if target and target.name == ("npc_dota_hero_sand_king") and (target.health/target.maxHealth < 0.3) then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-				return
-			else
-			    return
-		    end
-     	end
+			return
+		end
 		
 		if target and target.name == ("npc_dota_hero_nyx_assassin") and (target:GetAbility(4).CanBeCasted()) then
-	        if dust and dust:CanBeCasted() then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-				return
-			else
-			    return
-		    end
+			return
+		end
      	end
 	    
 end
