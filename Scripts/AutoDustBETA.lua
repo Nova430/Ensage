4d2b31d89fd10fe332a55887988f4c4430d504b5
NovaHF <nova@live.co.uk>
2015-03-26 21:55:33 +0000

    Update AutoDustBETA.lua

--- Scripts/AutoDustBETA.lua
+++ Scripts/AutoDustBETA.lua
@@ -33,70 +33,54 @@ function Main(tick)
 	local me = entityList:GetMyHero()
 	if not me then return end
 	local dust = me:FindItem("item_dust")
-	
-        if dust and dust:CanBeCasted() then
-        	FindTarget()
-    	        if target and (target:DoesHaveModifier("modifier_bounty_hunter_wind_walk") 
-    	        	      or target:DoesHaveModifier("modifier_riki_permanent_invisibility") 
-    	        	      or target:DoesHaveModifier("modifier_mirana_moonlight_shadow") 
-    	        	      or target:DoesHaveModifier("modifier_treant_natures_guise") 
-    	        	      or target:DoesHaveModifier("modifier_weaver_shukuchi") 
-    	        	      or target:DoesHaveModifier("modifier_broodmother_spin_web_invisible_applier") 
-    	        	      or target:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") 
-    	        	      or target:DoesHaveModifier("modifier_rune_invis") 
-    	        	      or target:DoesHaveModifier("modifier_clinkz_wind_walk") 
-    	        	      or target:DoesHaveModifier("modifier_item_shadow_amulet_fade")) 
-    	        	      and not (target:DoesHaveModifier("modifier_bounty_hunter_track") 
-    	        	        	or target:DoesHaveModifier("modifier_bloodseeker_thirst_vision") 
-    	        	        	or target:DoesHaveModifier("modifier_slardar_amplify_damage") 
-    	        	        	or target:DoesHaveModifier("modifier_item_dustofappearance")) then
+
+    if dust and dust:CanBeCasted() then
+	    local enemies = entityList:GetEntities(function(v) return v.type == LuaEntity.TYPE_HERO and v.team == me:GetEnemyTeam() and v.visible and not v.illusion and v.alive and v:GetDistance2D(me) <= range end)
+		for i,target in ipairs(enemies) do
+    	    if target and (target:DoesHaveModifier("modifier_bounty_hunter_wind_walk") 
+    	    	      or target:DoesHaveModifier("modifier_riki_permanent_invisibility") 
+    	       	      or target:DoesHaveModifier("modifier_mirana_moonlight_shadow") 
+    	       	      or target:DoesHaveModifier("modifier_treant_natures_guise") 
+    	       	      or target:DoesHaveModifier("modifier_weaver_shukuchi") 
+    	       	      or target:DoesHaveModifier("modifier_broodmother_spin_web_invisible_applier") 
+    	       	      or target:DoesHaveModifier("modifier_item_invisibility_edge_windwalk") 
+    	       	      or target:DoesHaveModifier("modifier_rune_invis") 
+    	       	      or target:DoesHaveModifier("modifier_clinkz_wind_walk") 
+    	       	      or target:DoesHaveModifier("modifier_item_shadow_amulet_fade")) 
+    	       	      and not (target:DoesHaveModifier("modifier_bounty_hunter_track") 
+    	      	        	or target:DoesHaveModifier("modifier_bloodseeker_thirst_vision") 
+    	       	        	or target:DoesHaveModifier("modifier_slardar_amplify_damage") 
+    	       	        	or target:DoesHaveModifier("modifier_item_dustofappearance")) then
 		        me:CastAbility(dust)
 		        Sleep(30000)
-			return
+		    return
 	        end
 		
-		if me and me:DoesHaveModifier("modifier_invoker_ghost_walk_enemy") then
-		        me:CastAbility(dust)
-		        Sleep(30000)
-			return
-		end
+		    if me and me:DoesHaveModifier("modifier_invoker_ghost_walk_enemy") then
+		            me:CastAbility(dust)
+		            Sleep(30000)
+			    return
+		    end
 		
-		if target and target.name == ("npc_dota_hero_templar_assassin") and (target.health/target.maxHealth < 0.3) then
-		        me:CastAbility(dust)
-		        Sleep(30000)
-			return
-		end
+		    if target and target.name == ("npc_dota_hero_templar_assassin") and (target.health/target.maxHealth < 0.3) then
+		            me:CastAbility(dust)
+		            Sleep(30000)
+			    return
+		    end
 		
-		if target and target.name == ("npc_dota_hero_sand_king") and (target.health/target.maxHealth < 0.3) then
-		        me:CastAbility(dust)
-		        Sleep(30000)
-			return
-		end
+	    	if target and target.name == ("npc_dota_hero_sand_king") and (target.health/target.maxHealth < 0.3) then
+		            me:CastAbility(dust)
+		            Sleep(30000)
+		    	return
+		    end
 		
-		if target and target.name == ("npc_dota_hero_nyx_assassin") and (target:GetAbility(4).CanBeCasted()) then
-		        me:CastAbility(dust)
-		        Sleep(30000)
-			return
-		end
-     	end
-	    
-end
-
-function FindTarget()
-	local me = entityList:GetMyHero()
-	local enemies = entityList:FindEntities({type=LuaEntity.TYPE_HERO,team = me:GetEnemyTeam(),alive=true,visible=true,illusion=false})
-	local InvisTarget = nil
-	for i,v in ipairs(enemies) do
-		distance = GetDistance2D(v,me)
-		if distance <= range then 
-			if InvisTarget == nil then
-		        InvisTarget = v
-			elseif distance > range then
-			    InvisTarget = nil
+		    if target and target.name == ("npc_dota_hero_nyx_assassin") and target:GetAbility(4):CanBeCasted() then
+		            me:CastAbility(dust)
+		            Sleep(30000)
+		    	return
 		    end
 		end
-	end
-	target = InvisTarget
+   	end  
 end
 
 
